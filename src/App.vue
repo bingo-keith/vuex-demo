164fe5fe2a21d61ba853c31b6aebec6aeca94e97
郑文斌 <3421736@qq.com>
2019-04-19 10:01:35 +0800

    add actions and component dispatch

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,40 @@
 <template>
   <div id="app">
     <router-view/>
+    <FooterBar v-if="isShow"/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  import FooterBar from "./components/FooterBar";
+
+  export default {
+    name: 'App',
+    components: {
+      FooterBar,
+    },
+    data() {
+      return {
+
+      }
+    },
+    computed: {
+      isShow() {
+        this.$store.dispatch('hideFooter');
+        console.log(this.$store.getters.isShow)
+        return this.$store.getters.isShow;
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/FooterBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      这是一个FooterBar
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "FooterBar"
+    }
+</script>
+
+<style scoped>
+
+</style>
